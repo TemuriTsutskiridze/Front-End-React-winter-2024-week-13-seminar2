d60265ddc9b4d3ceeb2366454c9c6dc0f93e5b32
Temuri Tsutskiridze <temuri.tsutskiridze.04@gmail.com>
2024-09-06 21:12:21 +0400

    redux toolkit

---
+++ src/store-v2.ts
@@ -0,0 +1,17 @@
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import accountReducer from "./features/accounts/accountSlice";
+import custumerReducer from "./features/customers/customerSlice";
+import { thunk } from "redux-thunk";
+
+const rootReducer = combineReducers({
+  account: accountReducer,
+  customer: custumerReducer,
+});
+
+export type RootState = ReturnType<typeof rootReducer>;
+
+export type AppDispatch = typeof store.dispatch;
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+export default store;
